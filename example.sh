acfef2479f5aa12f1344314eedd46ef7dc440170
EC2 Default User <ec2-user@ip-172-31-33-132.ap-south-1.compute.internal>
2024-05-17 13:02:40 +0000

    first commit

---
+++ example.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+files=`ls | wc -l`
+
+
+if [ $files -gt 10 ];
+then
+	ls -lrt
+else
+	pwd
+fi
---
+++ variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+name=sathya
+age=26
+colour=white 
+
+echo "my name is $name"
+echo "my age is $age"
+echo "my colour is $colour"
